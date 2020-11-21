d18fbbd90d63f4e6098aecd109763ccb4e98854a
V <vincent@platform45.com>
2020-11-21 17:14:33 +0200

    Update: JS sample snippet

--- api_call_sample.js
+++ api_call_sample.js
@@ -1,6 +1,6 @@
 const beforeTransaction = async (authorization) => {
   // create the url to call your api with only the merchant name and your user token
-  url = `https://our_api.endpount/get_rekt?merchant_name=${authorization.merchant}&user_api_token=${your_test_token_here}`
+  url = `https://our_api.endpoint/blacklisted?user_api_token=${your_test_token_here}&merchant_name=${authorization.merchant}`
 
   // call the api with the url
     const response = await fetch(url, {
