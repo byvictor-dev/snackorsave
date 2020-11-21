1e390fcf2d90b80cdcfa0805b226c31803617f36
V <vincent@platform45.com>
2020-11-21 17:11:03 +0200

    Add: specs

--- spec/requests/blacklists_controller_request_spec.rb
+++ spec/requests/blacklists_controller_request_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe "BlacklistsController", type: :request do
     end
   end
 
+  context '#create' do
+    let(:post_create) { post blacklists_path, params: {blacklist: blacklist.attributes.except(:user_id)} }
+    let(:blacklist)   { build :blacklist }
+
+    it 'creates a new blacklist item for valid params' do
+      expect{ post_create }.to change{ Blacklist.count }.by 1
+    end
+  end
+
+  context '#update' do
+    let(:patch_update) { patch blacklist_path(blacklist), params: {blacklist: {merchant_name: 'potatoes'}} }
+
+    it 'updates the blacklist item' do
+      expect{ patch_update }.to change{ blacklist.reload.merchant_name }.to 'potatoes'
+    end
+  end
+
 end
