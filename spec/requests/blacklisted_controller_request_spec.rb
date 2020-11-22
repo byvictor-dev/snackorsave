baf2094fddde622c3f01be36d8f97f47fe9ca782
jonathan@platform45.com <jonathan@platform45.com>
2020-11-22 12:30:57 +0200

    Refactored controller and tests

--- spec/requests/blacklisted_controller_request_spec.rb
+++ spec/requests/blacklisted_controller_request_spec.rb
@@ -1,53 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe BlacklistedController, type: :request do
-  let(:user)        { create :user,  api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4' }
-  let(:blacklist)   { create :blacklist, user: user, merchant_name: 'Bad Joo Joo' }
-  let(:blacklist_2) { create :blacklist, user: user, merchant_name: 'Good Joo Joo' }
-  let(:params) { {user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4', merchant_name: 'Bad Joo Joo'} }
+  let!(:user)                { create :user,  api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4' }
+  let!(:blocked_merchant)    { create :blacklist, user: user, merchant_name: 'Bad Joo Joo' }
+  let!(:authorized_merchant) { create :blacklist, user: user, merchant_name: 'Good Joo Joo', blocked: false }
 
   describe 'GET is_blacklisted' do
-    let(:get_is_blacklisted) {
-      get is_blacklisted_path,
-      params: params
-    }
+    context 'user does not exist' do
+      let(:get_is_blacklisted) {
+        get is_blacklisted_path,
+        params: {user_api_token: 'asdf'}
+      }
 
-    context 'user_api_token and merchant_name params are valid' do
-      it 'expects merchant to be unauthorized' do
+      it 'should respond with 404' do
         get_is_blacklisted
-        expect(JSON.parse(response.body)).to eq({'authorized'=> false})
-      end
-
-      it 'expects merchant to be authorized' do
-        get_is_blacklisted
-        expect(JSON.parse(response.body)).to eq({'authorized'=> false})
+        expect(response.status).to eq(404)
+        expect(JSON.parse(response.body)).to eq({})
       end
     end
 
-    context 'user_api_token param is invalid' do
-      let(:params) { {merchant_name: 'Bad Joo Joo'} }
+    context 'user exists' do
+      context 'blacklist does not exist' do
+        let(:get_is_blacklisted) {
+          get is_blacklisted_path,
+          params: {user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4'}
+        }
 
-      it 'expects merchant to be unauthorized' do
-        get_is_blacklisted
-        expect(JSON.parse(response.body)).to eq({'authorized'=> false})
+        it 'should respond with authorized:true' do
+          get_is_blacklisted
+          expect(JSON.parse(response.body)).to eq({'authorized' => true})
+        end
       end
-    end
 
-    context 'merchant_name param is invalid' do
-      let(:params) { {user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4'} }
+      context 'blacklist exists' do
+        context 'merchant is blocked' do
+          let(:get_is_blacklisted) {
+            get is_blacklisted_path,
+            params: {
+              user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4',
+              merchant_name: 'Bad Joo Joo'
+            }
+          }
 
-      it 'expects merchant to be unauthorized' do
-        get_is_blacklisted
-        expect(JSON.parse(response.body)).to eq({'authorized'=> false})
-      end
-    end
+          it 'should respond with authorized:false' do
+            get_is_blacklisted
+            expect(JSON.parse(response.body)).to eq({'authorized' => false})
+          end
+        end
 
-    context 'merchant is not blocked' do
-      let(:params) { {user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4', merchant_name: 'Good Joo Joo'} }
+        context 'merchant is authorized' do
+          let(:get_is_blacklisted) {
+            get is_blacklisted_path,
+            params: {
+              user_api_token: '5ef19d5d-d0d9-421c-93cd-e7d0187d54c4',
+              merchant_name: 'Good Joo Joo'
+            }
+          }
 
-      it 'expects merchant to be authorized' do
-        get_is_blacklisted
-        expect(JSON.parse(response.body)).to eq({'authorized'=> false})
+          it 'should respond with authorized:true' do
+            get_is_blacklisted
+            expect(JSON.parse(response.body)).to eq({'authorized' => true})
+          end
+        end
       end
     end
   end
