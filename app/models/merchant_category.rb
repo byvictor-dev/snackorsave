2e0805bd90ed4de2a6b640fc9f6b30ceb56bc496
jonathan@platform45.com <jonathan@platform45.com>
2020-11-28 17:49:18 +0200

    Added merchant category to form

--- app/models/merchant_category.rb
+++ app/models/merchant_category.rb
@@ -1,4 +1,8 @@
 class MerchantCategory < ApplicationRecord
   has_many :blacklists, dependent: :destroy
   has_many :transaction_attempts, dependent: :destroy
+
+  def to_s
+    description
+  end
 end
