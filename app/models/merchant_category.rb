a71fbaef8ddc748ddcae8b5389d0c913370d2808
jonathan@platform45.com <jonathan@platform45.com>
2020-11-28 17:58:54 +0200

    Removed to_s and changed reference to merchant description

--- app/models/merchant_category.rb
+++ app/models/merchant_category.rb
@@ -1,8 +1,4 @@
 class MerchantCategory < ApplicationRecord
   has_many :blacklists, dependent: :destroy
   has_many :transaction_attempts, dependent: :destroy
-
-  def to_s
-    description
-  end
 end
